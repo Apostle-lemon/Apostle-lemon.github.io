3be4c2ab6ca519f6058ff1b48dc72263195b7a76
lemon <1178483226@qq.com>
2022-11-04 19:09:07 +0800

    update through script at 2022-11-04 19:09:07

---
+++ test.bat
@@ -0,0 +1,18 @@
+:: %time%输出时间，10点前，小时只有1位，如9:01
+:: %date%输出的月、日，%time%输出的分、秒已自动加上0
+@echo off
+if "%time%" lss "10:00" (
+    ::%date:~0,4%表示取%date%从第0位开始的4位
+    ::系统时间格式设置可能不同，可根据时间格式修改%date%、%time%取值位置
+    set now=%date:~0,4%-%date:~5,2%-%date:~8,2% 0%time:~1,1%:%time:~3,2%:%time:~6,2%
+) else (
+    set now=%date:~0,4%-%date:~5,2%-%date:~8,2% %time:~0,2%:%time:~3,2%:%time:~6,2%
+)
+
+git add .
+
+git commit -m "update through script at %now%"
+
+git push
+
+echo "%now% update success"
